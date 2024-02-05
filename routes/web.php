68de43eda4160dbce0ca26ad47c3929d29e543a9
sidikwandi <147595583+sidikwandi@users.noreply.github.com>
2024-02-05 15:54:58 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/audit', 'App\Http\Controllers\PerencanaanAuditController@index')->middleware(['auth', 'verified'])->name('home');
+
 require __DIR__.'/auth.php';
