520fdefd7afce4b643039fc0074d3ec4a37452b4
stepani212 <147595583+sidikwandi@users.noreply.github.com>
2024-02-05 15:28:45 +0700

    Update v_sidebar.blade.php

---
+++ resources/views/layouts/v_sidebar.blade.php
@@ -0,0 +1,19 @@
+<aside id="sidebar" class="sidebar">
+
+    <ul class="sidebar-nav" id="sidebar-nav">
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="/audit">
+          <i class="bi bi-grid"></i>
+          <span>Perencanaan Audit</span>
+        </a>
+      </li><!-- End Dashboard Nav -->
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="/pelaksanaan">
+          <i class="bi bi-clock"></i>
+          <span>Pelaksanaan Audit</span>
+        </a>
+      </li><!-- End Dashboard Nav -->
+    </ul>
+
+  </aside>
